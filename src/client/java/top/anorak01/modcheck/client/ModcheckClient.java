b4317564de5887e19bc18a0b2ebe10d8a32f54a2
Anorak01 <adamfri007@gmail.com>
2024-07-27 18:45:37 +0200

    Complete rework of the mod All negotiations are now happening in the Login phase of connecting to the server. This makes it faster and safer and doesn't allow for even a brief period of time where the client can join and wreck havoc.

--- src/client/java/top/anorak01/modcheck/client/ModcheckClient.java
+++ src/client/java/top/anorak01/modcheck/client/ModcheckClient.java
@@ -3,6 +3,7 @@ package top.anorak01.modcheck.client;
 import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.loader.api.FabricLoader;
@@ -10,64 +11,31 @@ import net.minecraft.network.PacketByteBuf;
 import top.anorak01.modcheck.Modcheck;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import top.anorak01.modcheck.NetworkHandler;
 
 public class ModcheckClient implements ClientModInitializer {
-    private boolean modCheckSent = false;
-
-
     @Override
     public void onInitializeClient() {
-        registerModcheckClientJoin();
-        registerModCheckHandshake();
+        registerModcheckResponder();
     }
 
-    private static void sendChecksumsToServer(Map<String, String> checksums) {
-        PacketByteBuf buf = PacketByteBufs.create();
-        buf.writeInt(checksums.size());
-        checksums.forEach((fileName, checksum) -> {
-            buf.writeString(fileName);
-            buf.writeString(checksum);
-        });
+    private static void registerModcheckResponder() {
+        ClientLoginNetworking.registerGlobalReceiver(NetworkHandler.MOD_CHECK_MODLIST_CHANNEL, (((client, handler, buf, listenerAdder) -> {
+            Modcheck.LOGGER.info("Sending modlist");
 
-        ClientPlayNetworking.send(NetworkHandler.CHECKSUMS_PACKET_ID, buf);
-    }
+            PacketByteBuf responseBuf = PacketByteBufs.create();
 
-    private static void registerModcheckClientJoin() {
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            Modcheck.LOGGER.info("Sending modlist to server");
-            Modcheck.regenerateModlist();
-            sendChecksumsToServer(Modcheck.modlist_w_checksums);
-        });
-    }
+            Map<String, String> checksums = Modcheck.modlist_w_checksums;
+            responseBuf.writeInt(checksums.size());
+            checksums.forEach((fileName, checksum) -> {
+                responseBuf.writeString(fileName);
+                responseBuf.writeString(checksum);
+            });
 
-    private void registerModCheckHandshake() {
-        ClientPlayNetworking.registerGlobalReceiver(NetworkHandler.MOD_CHECK_REQUEST, ((client, handler, buf, responseSender) -> {
-            modCheckSent=false;
-            if (client.player != null && client.world != null) {
-                boolean hasMod = FabricLoader.getInstance().isModLoaded("modcheck");
-
-                PacketByteBuf responseBuf = PacketByteBufs.create();
-                responseBuf.writeBoolean(hasMod);
-
-                ClientPlayNetworking.send(NetworkHandler.MOD_CHECK_RESPONSE, responseBuf);
-            }
-        }
-        ));
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null && !modCheckSent) {
-                Modcheck.LOGGER.info("Sending handshake");
-                boolean hasMod = FabricLoader.getInstance().isModLoaded("modcheck");
-
-                PacketByteBuf responseBuf = PacketByteBufs.create();
-                responseBuf.writeBoolean(hasMod);
-
-                ClientPlayNetworking.send(NetworkHandler.MOD_CHECK_RESPONSE, responseBuf);
-
-                modCheckSent = true;
-            }
-        });
+            return CompletableFuture.completedFuture(responseBuf);
+        })));
     }
 }
--- src/main/java/top/anorak01/modcheck/NetworkHandler.java
+++ src/main/java/top/anorak01/modcheck/NetworkHandler.java
@@ -1,85 +1,58 @@
 package top.anorak01.modcheck;
 
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.*;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
-import static top.anorak01.modcheck.Modcheck.modCheckResponses;
-import static top.anorak01.modcheck.Modcheck.scheduler;
+import static top.anorak01.modcheck.Modcheck.*;
 
 public class NetworkHandler {
-    public static final Identifier CHECKSUMS_PACKET_ID = new Identifier("modcheck", "checksums");
-    public static final Identifier MOD_CHECK_REQUEST = new Identifier("modcheck", "mod_check_request");
-    public static final Identifier MOD_CHECK_RESPONSE = new Identifier("modcheck", "mod_check_response");
-
+    public static final Identifier MOD_CHECK_MODLIST_CHANNEL = new Identifier("modcheck", "mod_check_modlist_channel");
 
     public static void register() {
         Modcheck.LOGGER.info("Registering network handlers");
-        registerChecksumPacket();
-        registerHandshakeWorkflow();
+        registerModlistHandler();
     }
 
-    private static void registerHandshakeWorkflow() {
-        ServerPlayNetworking.registerGlobalReceiver(MOD_CHECK_RESPONSE, (server, player, handler, buf, responseSender) -> {
-            boolean hasMod = buf.readBoolean();
 
-            if (!hasMod) {
-                player.networkHandler.disconnect(Text.of("You don't have ModCheck mod installed!"));
-            }
-            Modcheck.LOGGER.info("Received ModCheck handshake from {}", player.getName().getString());
-            modCheckResponses.put(player.getUuid(), true);
-        });
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            ServerPlayerEntity player = handler.getPlayer();
-            modCheckResponses.put(player.getUuid(), false);
-            sendModCheckRequest(player);
-            Modcheck.LOGGER.info("Sending handshake request to {}", player.getName().getString());
-
-            // Schedule a task to check if the client responded after 5 seconds
-            scheduler.schedule(() -> {
-                if (!modCheckResponses.get(player.getUuid())) {
-                    player.networkHandler.disconnect(Text.of("You don't have ModCheck mod installed!"));
+    private static void registerModlistHandler(){
+        ServerLoginNetworking.registerGlobalReceiver(MOD_CHECK_MODLIST_CHANNEL, ((server, handler, understood, buf, synchronizer, responseSender) -> {
+            if (!understood) {
+                handler.disconnect(Text.of("You don't seem to have the ModCheck mod installed"));
+            } else {
+                int bufLen = buf.readInt();
+                Map<String, String> modlist = new HashMap<>();
+                for (int i = 0; i < bufLen; i++) {
+                    String modname = buf.readString();
+                    String checksum = buf.readString();
+                    modlist.put(modname, checksum);
                 }
-            }, 5, TimeUnit.SECONDS);
-        });
-    }
 
-    private static void registerChecksumPacket() {
-        ServerPlayNetworking.registerGlobalReceiver(CHECKSUMS_PACKET_ID, (server, player, handler, buf, responseSender) -> {
-            int count = buf.readInt();
-            Map<String, String> checksums = new HashMap<>();
-            for (int i = 0; i < count; i++) {
-                String fileName = buf.readString(32767);
-                String checksum = buf.readString(32767);
-                checksums.put(fileName, checksum);
+                if (modlist.equals(Modcheck.modlist_w_checksums)) {
+                    String conInfo = handler.getConnectionInfo();
+                    String name = conInfo.substring(conInfo.indexOf("name=")+5, conInfo.indexOf(",", conInfo.indexOf("name=")+5));
+                    String uuid = conInfo.substring(conInfo.indexOf("id=")+3, conInfo.indexOf(","));
+
+                    LOGGER.info("Modlist verified for {}: {}", name, uuid);
+                } else {
+                    handler.disconnect(Text.of("Your mods don't match with the expected modlist"));
+                }
             }
-            validateChecksums(player, checksums);
-        });
-    }
-
-    private static void validateChecksums(ServerPlayerEntity player, Map<String, String> checksums) {
-        // Your validation logic here
-        // Disconnect player if validation fails
-        Modcheck.LOGGER.info("Received mods from: {} valid: {}", player.getName().getString(), checksums.equals(Modcheck.modlist_w_checksums));
-        boolean valid = checksums.equals(Modcheck.modlist_w_checksums); // Replace with actual validation logic
-        if (!valid) {
-            Text reason = Text.of("Your mods don't match with what's expected.");
-            player.networkHandler.disconnect(reason);
         }
+        ));
+        ServerLoginConnectionEvents.QUERY_START.register(((handler, server1, sender, synchronizer) -> {
+            String conInfo = handler.getConnectionInfo();
+            String name = conInfo.substring(conInfo.indexOf("name=")+5, conInfo.indexOf(",", conInfo.indexOf("name=")+5));
+            String uuid = conInfo.substring(conInfo.indexOf("id=")+3, conInfo.indexOf(","));
+
+            PacketByteBuf buf = PacketByteBufs.create();
+            sender.sendPacket(MOD_CHECK_MODLIST_CHANNEL, buf);
+
+            Modcheck.LOGGER.info("Sending modlist request to {}: {}", name, uuid);
+        }
+        ));
     }
-
-    private static void sendModCheckRequest(ServerPlayerEntity player) {
-        PacketByteBuf buf = PacketByteBufs.create();
-        ServerPlayNetworking.send(player, MOD_CHECK_REQUEST, buf);
-    }
-
-
 }
